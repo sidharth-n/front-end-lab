c88f4baa8c731f23741f98b3f857dabc14f54892
sidharth-n <sidharthnofficial@gmail.com>
2022-11-30 13:29:01 +0530

    update exercise 2

--- Apple-music/debug.js
+++ Apple-music/debug.js
@@ -1,28 +1,10 @@
-const input = document.querySelector("input");
-const overlay = document.querySelector(".disabled");
-const slider = document.querySelector("#slider");
-const main_container = document.querySelector(".main-container");
-const play_box = document.querySelector(".play-box");
-let val = "work";
-document.addEventListener("keydown", (event) => {
-  const name = event.key;
-  if (name == "d") {
-    if (val == "work") {
-      overlay.setAttribute("class", "overlay");
-      slider.style.display = "block";
-      val = "debug";
-    } else {
-      overlay.setAttribute("class", "disabled");
-      slider.style.display = "none";
-      val = "work";
-      main_container.style.opacity = "1";
-      play_box.style.opacity = "1";
-    }
-  }
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
 });
-
-slider.addEventListener("input", () => {
-  overlay.style.opacity = slider.value;
-  main_container.style.opacity = 1 - slider.value;
-  play_box.style.opacity = 1 - slider.value;
-});
+const value = params.mode;
+const debug_nav = document.querySelector(".debug-nav");
+if (value == "1") {
+  debug_nav.style.display = "flex";
+} else {
+  debug_nav.style.display = "none";
+}
