387941add6872c94fcaaced114cba41516ccd269
sidharth-n <sidharthnofficial@gmail.com>
2022-12-01 09:14:45 +0530

    exercise 2 update

--- Apple-music/debug.js
+++ Apple-music/debug.js
@@ -1,9 +1,6 @@
-const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, prop) => searchParams.get(prop),
-});
-const value = params.mode;
+const value = window.location.search;
 const debug_nav = document.querySelector(".debug-nav");
-if (value == "1") {
+if (value.includes("debug")) {
   debug_nav.style.display = "flex";
 } else {
   debug_nav.style.display = "none";
