a7d83d7d27dde5bfb5872a7559a71962fd2792ca
sidharth-n <sidharthnofficial@gmail.com>
2022-12-21 14:15:35 +0530

    w-chat upto color chat updated

--- Whatapp-chat/bot.js
+++ Whatapp-chat/bot.js
@@ -3,9 +3,32 @@ const s_btn = document.querySelector(".send-btn");
 const r_msg = document.querySelector(".r-msg");
 const s_msg = document.querySelector(".s-msg");
 const main_con = document.querySelector(".main-container");
+document.querySelector(".r1").remove();
+document.querySelector(".r2").remove();
+text_area.focus();
 let msg = "";
 let pos = 0;
 let started = 0;
+let prev_rand = -1;
+
+const rand_colors = [
+  "rgb(180, 243, 244)",
+  "rgb(255, 153, 153)",
+  "rgb(242, 242, 145)",
+  "rgb(234, 139, 139)",
+  "rgb(175, 175, 238)",
+  "rgb(242, 182, 125)",
+  "rgb(225, 227, 146)",
+];
+
+const random_color = () => {
+  let rand = Math.floor(Math.random() * 8);
+  if (rand == prev_rand) {
+    rand_colors();
+  }
+  prev_rand = rand;
+  return rand_colors[rand];
+};
 
 const fetcher = () => {
   const url = `https://textdb.dev/api/data/sid123007`;
@@ -55,6 +78,7 @@ const add_msg = (new_msg, s_time) => {
   time_el.textContent = s_time;
   main_con.appendChild(msg_el);
   msg_el.appendChild(time_el);
+  msg_el.style.backgroundColor = random_color();
 };
 
 s_btn.addEventListener("click", () => {
