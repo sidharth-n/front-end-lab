18a82baf14b6db15c5e840faa9045b47e49b91b6
sidharth-n <sidharthnofficial@gmail.com>
2022-12-27 14:39:52 +0530

    timer function done

--- pomorodo-timer/script.js
+++ pomorodo-timer/script.js
@@ -1,5 +1,9 @@
 const main_div = document.querySelector(".main");
-
+const startBtn = document.querySelector(".start-stop");
+const timerMinutes = document.querySelector(".t-min");
+const timerSeconds = document.querySelector(".t-secs");
+const taskName = document.querySelector(".current-task");
+const timerCircle = document.querySelector(".timer-circle");
 const vw = Math.max(
   document.documentElement.clientWidth || 0,
   window.innerWidth || 0
@@ -11,3 +15,38 @@ const vh = Math.max(
 
 main_div.style.width = `${vw}px`;
 main_div.style.height = `${vh}px`;
+let minutes = Number(timerMinutes.innerText);
+let seconds = Number(timerSeconds.innerText);
+const totalTime = minutes * 60 + seconds;
+const digreePerSecond = 360 / totalTime;
+console.log(digreePerSecond);
+
+let countdown = () => {
+  let count = 1;
+  let timerDown = setInterval(() => {
+    timerCircle.style.background = `conic-gradient(#4742c0, ${
+      digreePerSecond * count
+    }deg, #2f2d38 0deg)`;
+    if (seconds == 1 && minutes == 0) {
+      clearInterval(timerDown);
+      taskName.style.backgroundColor = "green";
+    }
+    if (minutes != 0) {
+      if (seconds == 0) {
+        seconds = 60;
+      }
+    }
+    seconds--;
+    if (seconds == 59) {
+      minutes--;
+      timerMinutes.innerText = minutes >= 10 ? minutes : `0${minutes}`;
+    }
+    timerSeconds.innerText = seconds >= 10 ? seconds : `0${seconds}`;
+    count++;
+    /*     timerCircle.style.background = `conic-gradient(#4742c0, 32.72deg, #2f2d38 0deg);`; */
+  }, 1000);
+};
+
+startBtn.addEventListener("click", () => {
+  countdown();
+});
