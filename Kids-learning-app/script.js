7357a7410dac0052a0071418a60452db6f91e1ee
sidharth-n <sidharthnofficial@gmail.com>
2022-12-29 12:10:22 +0530

    animal sounds added screen 2 added first version done

--- Kids-learning-app/script.js
+++ Kids-learning-app/script.js
@@ -1,5 +1,50 @@
+const value = window.location.search;
+const debug_nav = document.querySelector(".debug-nav");
 if (value.includes("debug")) {
   debug_nav.style.display = "flex";
 } else {
   debug_nav.style.display = "none";
 }
+
+const back = document.querySelector(".back");
+const animalsListPage = document.querySelector(".animals-list-page");
+const animalsDetailPage = document.querySelector(".animal-details-page");
+const lionCard = document.querySelector(".a1");
+const lionPlayBtn = document.querySelector(".play-lion");
+const catPlayBtn = document.querySelector(".play-cat");
+const horsePlayBtn = document.querySelector(".play-horse");
+const dogPlayBtn = document.querySelector(".play-dog");
+const monkeyPlayBtn = document.querySelector(".play-monkey");
+const catImage = document.querySelector(".a2 > .animal-image");
+
+back.addEventListener("click", () => {
+  animalsDetailPage.style.display = "none";
+  animalsListPage.style.display = "flex";
+});
+
+lionCard.addEventListener("click", () => {
+  animalsDetailPage.style.display = "flex";
+  animalsListPage.style.display = "none";
+});
+
+lionPlayBtn.addEventListener("click", () => {
+  new Audio("./assets/sounds/lion.wav").play();
+});
+
+catPlayBtn.addEventListener("click", () => {
+  new Audio("./assets/sounds/cat.wav").play();
+  catImage.style.rotate = "20deg";
+  catImage.style.rotate = "-20deg";
+});
+
+horsePlayBtn.addEventListener("click", () => {
+  new Audio("./assets/sounds/horse.wav").play();
+});
+
+dogPlayBtn.addEventListener("click", () => {
+  new Audio("./assets/sounds/dog.wav").play();
+});
+
+monkeyPlayBtn.addEventListener("click", () => {
+  new Audio("./assets/sounds/monkey.wav").play();
+});
