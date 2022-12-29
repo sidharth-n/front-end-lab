902a338f7a41092eb50f5d5d2981984843aaa290
sidharth-n <sidharthnofficial@gmail.com>
2022-12-29 21:15:16 +0530

    password page done

--- google-signin/script.js
+++ google-signin/script.js
@@ -7,13 +7,23 @@ if (value.includes("debug")) {
 }
 const mainDiv = document.querySelector(".main");
 const emailInput = document.querySelector(".email-input");
+const passInput = document.querySelector("#password-input");
 const nextBtn = document.querySelector(".next-btn");
+const nextBtn2 = document.querySelector(".nxt2");
 const warningEmail = document.querySelector(".warning-email");
+const warningPass = document.querySelector(".warning-password");
 const focusText = document.querySelector(".focus-text");
+const focusText2 = document.querySelector(".f2");
+const emailPage = document.querySelector(".p1");
+const passPage = document.querySelector(".p2");
+const emailBox = document.querySelector(".email-address");
+const showPassCheck = document.querySelector(".checkbox");
 const vh = Math.max(
   document.documentElement.clientHeight || 0,
   window.innerHeight || 0
 );
+let account = {};
+
 mainDiv.style.height = `${vh}px`;
 nextBtn.addEventListener("click", () => {
   if (emailInput.value == "") {
@@ -21,5 +31,22 @@ nextBtn.addEventListener("click", () => {
     focusText.style.color = "red";
     emailInput.style.border = "1px solid red";
     warningEmail.style.display = "flex";
+  } else {
+    emailPage.style.display = "none";
+    passPage.style.display = "flex";
+    account.email = emailInput.value;
+    emailBox.innerText = emailInput.value;
+  }
+});
+
+nextBtn2.addEventListener("click", () => {
+  if (passInput.value == "") {
+    focusText2.style.display = "block";
+    focusText2.style.color = "red";
+    passInput.style.border = "1px solid red";
+    warningPass.style.display = "flex";
+  } else {
+    account.passsword = passInput.value;
+    console.log(account);
   }
 });
