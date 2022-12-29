21c04a43e9850951a600687ecf92f88216acf687
sidharth-n <sidharthnofficial@gmail.com>
2022-12-29 15:24:39 +0530

    email empty warning enabled

--- google-signin/script.js
+++ google-signin/script.js
@@ -6,3 +6,20 @@ if (value.includes("debug")) {
   debug_nav.style.display = "none";
 }
 const mainDiv = document.querySelector(".main");
+const emailInput = document.querySelector(".email-input");
+const nextBtn = document.querySelector(".next-btn");
+const warningEmail = document.querySelector(".warning-email");
+const focusText = document.querySelector(".focus-text");
+const vh = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0
+);
+mainDiv.style.height = `${vh}px`;
+nextBtn.addEventListener("click", () => {
+  if (emailInput.value == "") {
+    focusText.style.display = "block";
+    focusText.style.color = "red";
+    emailInput.style.border = "1px solid red";
+    warningEmail.style.display = "flex";
+  }
+});
