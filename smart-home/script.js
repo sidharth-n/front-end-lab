bc0633916ef62e51e2e62d504a211e4edc26c134
sidharth-n <sidharthnofficial@gmail.com>
2023-01-03 14:49:26 +0530

    page link done before meeting page 2 to be done

--- smart-home/script.js
+++ smart-home/script.js
@@ -1,6 +1,11 @@
 const value = window.location.search;
 const debug_nav = document.querySelector(".debug-nav");
 const mainDiv = document.querySelector(".main");
+const page2 = document.querySelector(".page-2");
+const page1 = document.querySelector(".page-1");
+const backBtn = document.querySelector(".back-btn");
+const lampCard = document.querySelector(".d1");
+
 if (value.includes("debug")) {
   debug_nav.style.display = "flex";
 } else {
@@ -11,3 +16,13 @@ const vh = Math.max(
   window.innerHeight || 0
 );
 mainDiv.style.height = `${vh}px`;
+
+lampCard.addEventListener("click", () => {
+  page2.style.display = "flex";
+  page1.style.display = "none";
+});
+
+backBtn.addEventListener("click", () => {
+  page2.style.display = "none";
+  page1.style.display = "flex";
+});
