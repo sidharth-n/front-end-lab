5cf135a89d469429408f038b03aff7d3e9a74462
sidharth-n <sidharthnofficial@gmail.com>
2023-01-04 14:31:57 +0530

    padding issue fixed + food-details toggle added

--- restaurant-menu/script.js
+++ restaurant-menu/script.js
@@ -4,6 +4,8 @@ const mainDiv = document.querySelector(".main");
 const checkoutBtn = document.querySelector(".checkout");
 const page1 = document.querySelector(".page-1");
 const page2 = document.querySelector(".page-2");
+const foodDetailsSection = document.querySelector(".food-details");
+const foodCard = document.querySelector(".food-card");
 if (value.includes("debug")) {
   debug_nav.style.display = "flex";
 } else {
@@ -18,4 +20,8 @@ mainDiv.style.height = `${vh}px`;
 checkoutBtn.addEventListener("click", () => {
   page1.style.display = "none";
   page2.style.display = "flex";
+});
+
+foodCard.addEventListener("click", () => {
+  foodDetailsSection.style.display = "flex";
 });
