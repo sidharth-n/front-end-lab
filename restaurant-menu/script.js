ac75c8ddbac32f86d3d144ecf4b093519fc0b0b8
sidharth-n <sidharthnofficial@gmail.com>
2023-01-04 14:11:00 +0530

    checkout btn first copy

--- restaurant-menu/script.js
+++ restaurant-menu/script.js
@@ -1,6 +1,9 @@
 const value = window.location.search;
 const debug_nav = document.querySelector(".debug-nav");
 const mainDiv = document.querySelector(".main");
+const checkoutBtn = document.querySelector(".checkout");
+const page1 = document.querySelector(".page-1");
+const page2 = document.querySelector(".page-2");
 if (value.includes("debug")) {
   debug_nav.style.display = "flex";
 } else {
@@ -11,3 +14,8 @@ const vh = Math.max(
   window.innerHeight || 0
 );
 mainDiv.style.height = `${vh}px`;
+
+checkoutBtn.addEventListener("click", () => {
+  page1.style.display = "none";
+  page2.style.display = "flex";
+});
