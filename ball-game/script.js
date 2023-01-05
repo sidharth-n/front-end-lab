e3ad542e6a3354d5ecee593a0b52e027faa2c30b
sidharth-n <sidharthnofficial@gmail.com>
2023-01-05 11:26:11 +0530

    ball border touching done

--- ball-game/script.js
+++ ball-game/script.js
@@ -3,6 +3,7 @@ const debug_nav = document.querySelector(".debug-nav");
 const mainDiv = document.querySelector(".main");
 const consoleBox = document.querySelector(".debug-box");
 const ball1 = document.querySelector(".b1");
+const ball2 = document.querySelector(".b2");
 let x, y, z, report;
 let sensor = new Gyroscope();
 if (value.includes("debug")) {
@@ -19,7 +20,7 @@ ball1.style.left = `${mainDiv.offsetWidth / 2}px`;
 ball1.style.top = `${mainDiv.offsetHeight / 2}px`;
 sensor.start();
 sensor.onreading = () => {
-  x = sensor.x * 100;
+  x = sensor.x * 150;
   y = sensor.y * 100;
   z = sensor.z * 100;
   report = `X-axis AV : ${x} <br>`;
@@ -28,8 +29,26 @@ sensor.onreading = () => {
   report += `Ball X : ${ball1.offsetLeft} <br>`;
   report += `Ball Y : ${ball1.offsetTop} <br>`;
   consoleBox.innerHTML = report;
-  ball1.style.left = `${ball1.offsetLeft + y < 0 ? 0 : ball1.offsetLeft + y}px`;
-  ball1.style.top = `${ball1.offsetTop + x < 0 ? 0 : ball1.offsetTop + x}px`;
+  ball1.style.left = `${
+    ball1.offsetLeft + y < 0
+      ? 0
+      : ball1.offsetLeft + y > mainDiv.offsetWidth - 30
+      ? mainDiv.offsetWidth - 30
+      : ball1.offsetLeft + y
+  }px`;
+  ball1.style.top = `${
+    ball1.offsetTop + x < 0
+      ? 0
+      : ball1.offsetTop + x > ball1.style.top - 30
+      ? ball1.style.top - 30
+      : ball1.offsetTop + x
+  }px`;
+  if (
+    ball1.offsetLeft == ball2.offsetLeft ||
+    ball1.offsetTop == ball2.offsetTop
+  ) {
+    consoleBox.innerHTML = "YOU WON !!!";
+  }
 };
 
 sensor.onerror = (event) => {
