eff4f019521805eef0777227de2f85b5b636a3bc
sidharth-n <sidharthnofficial@gmail.com>
2023-01-06 14:31:18 +0530

    page size done

--- my-plants-app/script.js
+++ my-plants-app/script.js
@@ -1,6 +1,10 @@
 const value = window.location.search;
 const debug_nav = document.querySelector(".debug-nav");
 const mainDiv = document.querySelector(".main");
+const page1 = document.querySelector(".page-1");
+const page2 = document.querySelector(".page-2");
+const pullBtn1 = document.querySelector(".hori2");
+const pullBtn2 = document.querySelector(".pull-device");
 if (value.includes("debug")) {
   debug_nav.style.display = "flex";
 } else {
@@ -11,3 +15,12 @@ const vh = Math.max(
   window.innerHeight || 0
 );
 mainDiv.style.height = `${vh}px`;
+
+pullBtn1.addEventListener("click", () => {
+  /* page1.style.position = "absolute"; */
+  page1.style.Margintop = "1000px";
+  /*  page2.style.marginTop = "-100px"; */
+  pullBtn1.style.backgroundColor = "blue";
+});
+
+pullBtn2.addEventListener("click", () => {});
