87f970940f40ba8fe680e3d59b7c46cdcde04b67
sidharth-n <sidharthnofficial@gmail.com>
2023-01-06 14:43:42 +0530

    basic version working

--- my-plants-app/script.js
+++ my-plants-app/script.js
@@ -5,6 +5,9 @@ const page1 = document.querySelector(".page-1");
 const page2 = document.querySelector(".page-2");
 const pullBtn1 = document.querySelector(".hori2");
 const pullBtn2 = document.querySelector(".pull-device");
+const moreBtn = document.querySelector(".more-btn");
+const moreDiv1 = document.querySelector(".b3");
+const moreDiv2 = document.querySelector(".b4");
 if (value.includes("debug")) {
   debug_nav.style.display = "flex";
 } else {
@@ -18,9 +21,22 @@ mainDiv.style.height = `${vh}px`;
 
 pullBtn1.addEventListener("click", () => {
   /* page1.style.position = "absolute"; */
-  page1.style.Margintop = "1000px";
+  page2.style.marginTop = "-100vh";
   /*  page2.style.marginTop = "-100px"; */
-  pullBtn1.style.backgroundColor = "blue";
+  /* pullBtn1.style.backgroundColor = "blue"; */
 });
 
-pullBtn2.addEventListener("click", () => {});
+pullBtn2.addEventListener("click", () => {
+  page2.style.marginTop = "0vh";
+});
+moreBtn.addEventListener("click", () => {
+  if (moreBtn.innerText == "+2") {
+    moreDiv1.style.marginTop = "0px";
+    moreDiv2.style.marginTop = "0px";
+    moreBtn.innerText = "-2";
+  } else if (moreBtn.innerText == "-2") {
+    moreDiv1.style.marginTop = "-65px";
+    moreDiv2.style.marginTop = "-65px";
+    moreBtn.innerText = "+2";
+  }
+});
