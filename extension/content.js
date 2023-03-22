bf508b81e3e7ff225ea19f8b7e0f83a2bfb3c73d
sidharth-n <sidharthnofficial@gmail.com>
2023-03-22 07:57:40 +0530

    css backup

--- extension/content.js
+++ extension/content.js
@@ -1,36 +1,6 @@
-/* function addHelloButton() {
-  const subscribeButton = document.querySelector("#subscribe-button");
-  if (!subscribeButton) return;
-
-  const helloButton = document.createElement("button");
-  helloButton.id = "askButton";
-  helloButton.innerText = "Ask";
-  subscribeButton.parentNode.insertBefore(
-    helloButton,
-    subscribeButton.nextSibling
-  );
-  helloButton.addEventListener("click", () => {
-    alert("button clicked");
-  });
+function closeDiv() {
+  popupDiv.remove();
 }
-
-// Observe changes to the body of the YouTube page
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    // Check if the subscribe button has been updated
-    if (mutation.target.id === "subscribe-button") {
-      if (document.querySelector("#askButton")) return;
-      addHelloButton();
-    }
-  });
-});
-
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-});
- */
-
 function addHelloButton() {
   const subscribeButton = document.querySelector("#subscribe-button");
   if (!subscribeButton) return;
@@ -45,8 +15,28 @@ function addHelloButton() {
   helloButton.addEventListener("click", () => {
     // Create a popup div
     const popupDiv = document.createElement("div");
-    popupDiv.id = "askButton";
     popupDiv.id = "popupDiv";
+
+    // Create an iframe element and set its properties
+    const iframe = document.createElement("iframe");
+    iframe.src = "https://quizzapp-v4.netlify.app/";
+    iframe.id = "iframe";
+    // Append the iframe to the popup div
+    popupDiv.appendChild(iframe);
+
+    // Create a close button element and append it to the popup div
+    const topBar = document.createElement("div");
+    topBar.id = "top-bar";
+    topBar.innerHTML = `<div id='title'>GPTube</div><button id='closeBtn' onClick='closeDiv()'><svg id='close-svg' xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+viewBox="0 0 24 24" 
+fill="none" stroke="currentColor" 
+stroke-width="2" stroke-linecap="round" 
+stroke-linejoin="round"><line x1="18" 
+y1="6" x2="6" y2="18"></line><line x1="6" 
+y1="6" x2="18" y2="18"></line>
+</svg>
+</button>`;
+    popupDiv.appendChild(topBar);
 
     // Make the popup div draggable
     let isDragging = false;
