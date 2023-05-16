17b78b62991d567d034dd02ad13405e86ec26c26
sidharth-n <sidharthnofficial@gmail.com>
2023-05-16 19:27:54 +0530

    wokring basic model

--- guru/guru/src/App.jsx
+++ guru/guru/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Quote from "./components/Quote";
+import Quote from "./components/Quote.jsx";
 
 function App() {
   const [issue, setIssue] = useState("");
@@ -12,36 +12,35 @@ function App() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const prompt = `Retrieve a quote from Bhagavad Gita relevant to the life issue: "${issue}". Provide a verse and a short explanation of how it addresses the issue.`;
+    const prompt = `Retrieve a quote from Bhagavad Gita relevant to the life issue: "${issue}". Provide a verse and a short explanation of how it addresses the issue. give them back as a json file which contains only two keys -one is the verse and next is its explanation`;
 
-    const response = await fetch(
-      "https://api.openai.com/v1/engines/davinci-codex/completions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer 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`,
-        },
-        body: JSON.stringify({
-          prompt,
-          max_tokens: 150,
-          n: 1,
-          stop: null,
-          temperature: 1,
-        }),
-      }
-    );
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer 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`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+        temperature: 0.7,
+      }),
+    });
 
     const data = await response.json();
-    console.log(data);
-    const result = data.choices[0].text.trim();
-    const [verse, explanation] = result.split("\n\n");
+
+    const result = data.choices[0].message.content;
+    //console.log(result);
+    const parsedResult = JSON.parse(result);
+    const { verse, explanation } = parsedResult;
+    console.log(verse);
+
     setQuote({ verse, explanation });
   };
 
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-4xl font-bold mt-8 mb-4">Spiritual App</h1>
+      <h1 className="text-4xl font-bold mt-8 mb-4">Guru</h1>
       <form onSubmit={handleSubmit} className="mb-8">
         <input
           type="text"
@@ -54,7 +53,7 @@ function App() {
           type="submit"
           className="bg-blue-500 text-white px-6 py-2 rounded font-bold"
         >
-          Get Advice
+          Seek Advice
         </button>
       </form>
       {quote.verse && (
@@ -65,3 +64,5 @@ function App() {
 }
 
 export default App;
+
+//Bearer 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
