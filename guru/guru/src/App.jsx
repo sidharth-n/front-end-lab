f551458827754c8656db6abfd29db98fd10191a6
sidharth-n <sidharthnofficial@gmail.com>
2023-05-17 09:03:56 +0530

    bible worig demo

--- guru/guru/src/App.jsx
+++ guru/guru/src/App.jsx
@@ -4,6 +4,7 @@ import Quote from "./components/Quote.jsx";
 function App() {
   const [issue, setIssue] = useState("");
   const [quote, setQuote] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (event) => {
     setIssue(event.target.value);
@@ -11,14 +12,15 @@ function App() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
 
-    const prompt = `Retrieve a quote from Bhagavad Gita relevant to the life issue: "${issue}". Provide a verse and a short explanation of how it addresses the issue. give them back as a json file which contains only two keys -one is the verse and next is its explanation`;
+    const prompt = `Retrieve a excerpt from bible relevant to the life issue: "${issue}". Provide the excerpt and a short explanation of how it addresses the issue. give them back as a json file which contains only two keys -one is the verse and next is its explanation`;
 
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer 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`,
+        Authorization: `Bearer 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`,
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
@@ -30,17 +32,17 @@ function App() {
     const data = await response.json();
 
     const result = data.choices[0].message.content;
-    //console.log(result);
+    console.log(result);
     const parsedResult = JSON.parse(result);
     const { verse, explanation } = parsedResult;
-    console.log(verse);
 
     setQuote({ verse, explanation });
+    setIsLoading(false);
   };
 
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-4xl font-bold mt-8 mb-4">Guru</h1>
+      <h1 className="text-4xl font-bold mt-8 mb-4">Divine Echo</h1>
       <form onSubmit={handleSubmit} className="mb-8">
         <input
           type="text"
@@ -56,8 +58,15 @@ function App() {
           Seek Advice
         </button>
       </form>
-      {quote.verse && (
-        <Quote verse={quote.verse} explanation={quote.explanation} />
+      {isLoading ? (
+        <div className="flex flex-col items-center">
+          <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+          <p>Retrieving ancient wisdom...</p>
+        </div>
+      ) : (
+        quote.verse && (
+          <Quote verse={quote.verse} explanation={quote.explanation} />
+        )
       )}
     </div>
   );
--- guru/guru/src/components/Quote.jsx
+++ guru/guru/src/components/Quote.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Quote = ({ verse, explanation }) => {
   return (
-    <div className="bg-white shadow-md rounded p-6 mb-8">
-      <h2 className="text-xl font-bold mb-4">Verse:</h2>
-      <p className="text-gray-700 mb-4">{verse}</p>
-      <h2 className="text-xl font-bold mb-4">Explanation:</h2>
-      <p className="text-gray-700">{explanation}</p>
+    <div className="bg-white shadow-md rounded p-4 mb-8">
+      <h2 className="text-xl font-bold mb-3">Excerpt: </h2>
+      <p className="text-gray-700 mb-8 text-lg">{verse}</p>
+      <h2 className="text-xl font-bold mb-3">Explanation: </h2>
+      <p className="text-gray-700 text-lg">{explanation}</p>
     </div>
   );
 };
